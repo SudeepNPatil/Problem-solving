e704d581ff82049c79471a86d8faabd664bd50f7
SudeepNPatil <sudeepbsc02@gmail.com>
2025-10-17 00:53:36 +0530

    Day 12 of DSA challenge

--- Day 12/47th_sort_an_object_with_keys_alphabetics.js
+++ Day 12/47th_sort_an_object_with_keys_alphabetics.js
@@ -1,4 +1,41 @@
 function sortobjectbykey(object) {
+  let keys = [];
   for (let key in object) {
+    keys.push(key);
   }
+
+  function bubblesort(keys) {
+    for (let i = 0; i < keys.length; i++) {
+      let iswaped = false;
+      for (let j = 0; j < keys.length - i; j++) {
+        if (keys[j] > keys[j + 1]) {
+          let temp = keys[j];
+          keys[j] = keys[j + 1];
+          keys[j + 1] = temp;
+          iswaped = true;
+        }
+      }
+      if (!iswaped) {
+        return keys;
+      }
+    }
+    return keys;
+  }
+
+  let sorted = bubblesort(keys);
+  const sortedobject = {};
+  for (let i = 0; i < sorted.length; i++) {
+    sortedobject[sorted[i]] = object[sorted[i]];
+  }
+
+  return sortedobject;
 }
+
+let values = {
+  b: 1,
+  c: 2,
+  a: 3,
+  d: 4,
+};
+
+console.log(sortobjectbykey(values));
--- Day 12/48th_sort_object_by_its_value.js
+++ Day 12/48th_sort_object_by_its_value.js
@@ -1,4 +1,46 @@
 function sortbyvalue(object) {
+  let values = [];
   for (let key in object) {
+    values.push(object[key]);
   }
+
+  function bubblesort(values) {
+    for (let i = 0; i < values.length; i++) {
+      let isswaped = false;
+      for (let j = 0; j < values.length - i; j++) {
+        if (values[j] > values[j + 1]) {
+          let temp = values[j];
+          values[j] = values[j + 1];
+          values[j + 1] = temp;
+          isswaped = true;
+        }
+      }
+      if (!isswaped) {
+        return values;
+      }
+    }
+    return values;
+  }
+
+  let sortedvalues = bubblesort(values);
+
+  const sortedobject = {};
+  for (let i = 0; i < sortedvalues.length; i++) {
+    for (let key in object) {
+      if (object[key] === sortedvalues[i]) {
+        sortedobject[key] = sortedvalues[i];
+      }
+    }
+  }
+
+  return sortedobject;
 }
+
+let values = {
+  b: 2,
+  c: 3,
+  a: 1,
+  d: 4,
+};
+
+console.log(sortbyvalue(values));
