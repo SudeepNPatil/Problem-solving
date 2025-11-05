3750235b9a185e12e701210f737ec46a8d0b934c
SudeepNPatil <sudeepbsc02@gmail.com>
2025-11-05 23:02:07 +0530

    DAY 17 of DSA challange

--- Day 17/60th_Single_element_is_sorted_array.js
+++ Day 17/60th_Single_element_is_sorted_array.js
@@ -1,1 +1,20 @@
-function Singleelement(nums) {}
+function Singleelement(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (mid % 2 === 1) mid--;
+
+    if (nums[mid] === nums[mid + 1]) {
+      left = mid + 2;
+    } else {
+      right = mid;
+    }
+  }
+
+  return nums[left];
+}
+
+console.log(Singleelement([2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 7, 8, 8]));
